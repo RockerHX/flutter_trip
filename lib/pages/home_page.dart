f61c819797f5351d55f2fb9cca2ad211b209a239
RockerHX <rockerhx@gmail.com>
2020-05-17 18:58:24 +0800

    Update: 优化首页组件代码

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter_trip/dao/home_dao.dart';
 import 'package:flutter_trip/models/index.dart';
 import 'package:flutter_trip/widget/grid_nav.dart';
+import 'package:flutter_trip/widget/loading_container.dart';
 import 'package:flutter_trip/widget/local_nav.dart';
 import 'package:flutter_trip/widget/sales_box.dart';
 import 'package:flutter_trip/widget/sub_nav.dart';
@@ -17,6 +18,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isLoading = true;
   double appBarAlpha = 0;
   List<CommonModel> bannerList = [];
   List<CommonModel> localNavList = [];
@@ -27,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    loadData();
+    _handleRefresher();
   }
 
   _onscroll(offset) {
@@ -42,7 +44,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  loadData() {
+  Future<Null> _handleRefresher() async {
     HomeDao.fetch().then((model) {
       setState(() {
         bannerList = model.bannerList;
@@ -50,59 +52,84 @@ class _HomePageState extends State<HomePage> {
         gridNavModel = model.gridNav;
         subNavList = model.subNavList;
         salesBox = model.salesBox;
+        _isLoading = false;
       });
     }).catchError((e) {
       print(e);
+      setState(() {
+        _isLoading = false;
+      });
     });
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfff2f2f2),
-      body: Stack(
-        children: <Widget>[
-          MediaQuery.removePadding(
-            context: context,
-            removeTop: true,
-            child: NotificationListener(
-              onNotification: (notification) {
-                if (notification is ScrollUpdateNotification &&
-                    notification.depth == 0) {
-                  _onscroll(notification.metrics.pixels);
-                }
-                return true;
-              },
-              child: ListView(
-                children: <Widget>[
-                  _banner(),
-                  _localNav(),
-                  _gridNav(),
-                  _subNav(),
-                  _salesBox(),
-                ],
-              ),
-            ),
-          ),
-          Opacity(
-            opacity: appBarAlpha,
-            child: Container(
-              height: 100,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text('首页'),
+      body: LoadingContainer(
+        isLoading: _isLoading,
+        child: Stack(
+          children: <Widget>[
+            MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: NotificationListener(
+                onNotification: (notification) {
+                  if (notification is ScrollUpdateNotification &&
+                      notification.depth == 0) {
+                    _onscroll(notification.metrics.pixels);
+                  }
+                  return true;
+                },
+                child: RefreshIndicator(
+                  child: _listView,
+                  onRefresh: _handleRefresher,
                 ),
               ),
             ),
-          ),
-        ],
+            _appBar,
+          ],
+        ),
       ),
     );
   }
 
-  Widget _banner() {
+  Widget get _appBar {
+    return Opacity(
+      opacity: appBarAlpha,
+      child: Container(
+        height: 88,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.only(top: 40),
+            child: Text(
+              '首页',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget get _listView {
+    return ListView(
+      physics: const AlwaysScrollableScrollPhysics(),
+      children: <Widget>[
+        _banner,
+        _localNav,
+        _gridNav,
+        _subNav,
+        _salesBox,
+      ],
+    );
+  }
+
+  Widget get _banner {
     return Container(
       height: 200,
       child: Swiper(
@@ -132,28 +159,28 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _localNav() {
+  Widget get _localNav {
     return Padding(
       padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
       child: LocalNav(localNavList: localNavList),
     );
   }
 
-  Widget _gridNav() {
+  Widget get _gridNav {
     return Padding(
       padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
       child: GridNav(gridNavModel: gridNavModel),
     );
   }
 
-  Widget _subNav() {
+  Widget get _subNav {
     return Padding(
       padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
       child: SubNav(subNavList: subNavList),
     );
   }
 
-  Widget _salesBox() {
+  Widget get _salesBox {
     return Padding(
       padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
       child: SalesBox(salesBox: salesBox),
