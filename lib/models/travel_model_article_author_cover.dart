b4e4cf8db42284ef467a50b915e5109eaf16a28d
RockerHX <rockerhx@gmail.com>
2020-05-22 17:26:02 +0800

    Update[Model]: travel_model_article_author_cover

--- lib/models/travel_model_article_author_cover.dart
+++ lib/models/travel_model_article_author_cover.dart
@@ -1,41 +1,15 @@
 import 'package:json_annotation/json_annotation.dart';
 
-
-
 part 'travel_model_article_author_cover.g.dart';
 
 @JsonSerializable()
 class TravelModelArticleAuthorCover {
-      TravelModelArticleAuthorCover();
+  TravelModelArticleAuthorCover();
 
-  int articleId;
-  String articleType;
-  int productType;
-  int sourceType;
-  String articleTitle;
-  Map<String, dynamic> author;
-  List<dynamic> images;
-  bool hasVideo;
-  int readCount;
-  int likeCount;
-  int commentCount;
-  List<dynamic> urls;
-  List<dynamic> tags;
-  List<dynamic> topics;
-  List<dynamic> pois;
-  String publishTime;
-  String publishTimeDisplay;
-  String shootTime;
-  String shootTimeDisplay;
-  int level;
-  String distanceText;
-  bool isLike;
-  int imageCounts;
-  bool isCollected;
-  int collectCount;
-  int articleStatus;
-  String poiName;
+  String dynamicUrl;
+  String originalUrl;
 
-  factory TravelModelArticleAuthorCover.fromJson(Map<String,dynamic> json) => _$TravelModelArticleAuthorCoverFromJson(json);
+  factory TravelModelArticleAuthorCover.fromJson(Map<String, dynamic> json) =>
+      _$TravelModelArticleAuthorCoverFromJson(json);
   Map<String, dynamic> toJson() => _$TravelModelArticleAuthorCoverToJson(this);
 }
--- lib/models/travel_model_article_author_cover.g.dart
+++ lib/models/travel_model_article_author_cover.g.dart
@@ -9,63 +9,13 @@ part of 'travel_model_article_author_cover.dart';
 TravelModelArticleAuthorCover _$TravelModelArticleAuthorCoverFromJson(
     Map<String, dynamic> json) {
   return TravelModelArticleAuthorCover()
-    ..articleId = json['articleId'] as int
-    ..articleType = json['articleType'] as String
-    ..productType = json['productType'] as int
-    ..sourceType = json['sourceType'] as int
-    ..articleTitle = json['articleTitle'] as String
-    ..author = json['author'] as Map<String, dynamic>
-    ..images = json['images'] as List
-    ..hasVideo = json['hasVideo'] as bool
-    ..readCount = json['readCount'] as int
-    ..likeCount = json['likeCount'] as int
-    ..commentCount = json['commentCount'] as int
-    ..urls = json['urls'] as List
-    ..tags = json['tags'] as List
-    ..topics = json['topics'] as List
-    ..pois = json['pois'] as List
-    ..publishTime = json['publishTime'] as String
-    ..publishTimeDisplay = json['publishTimeDisplay'] as String
-    ..shootTime = json['shootTime'] as String
-    ..shootTimeDisplay = json['shootTimeDisplay'] as String
-    ..level = json['level'] as int
-    ..distanceText = json['distanceText'] as String
-    ..isLike = json['isLike'] as bool
-    ..imageCounts = json['imageCounts'] as int
-    ..isCollected = json['isCollected'] as bool
-    ..collectCount = json['collectCount'] as int
-    ..articleStatus = json['articleStatus'] as int
-    ..poiName = json['poiName'] as String;
+    ..dynamicUrl = json['dynamicUrl'] as String
+    ..originalUrl = json['originalUrl'] as String;
 }
 
 Map<String, dynamic> _$TravelModelArticleAuthorCoverToJson(
         TravelModelArticleAuthorCover instance) =>
     <String, dynamic>{
-      'articleId': instance.articleId,
-      'articleType': instance.articleType,
-      'productType': instance.productType,
-      'sourceType': instance.sourceType,
-      'articleTitle': instance.articleTitle,
-      'author': instance.author,
-      'images': instance.images,
-      'hasVideo': instance.hasVideo,
-      'readCount': instance.readCount,
-      'likeCount': instance.likeCount,
-      'commentCount': instance.commentCount,
-      'urls': instance.urls,
-      'tags': instance.tags,
-      'topics': instance.topics,
-      'pois': instance.pois,
-      'publishTime': instance.publishTime,
-      'publishTimeDisplay': instance.publishTimeDisplay,
-      'shootTime': instance.shootTime,
-      'shootTimeDisplay': instance.shootTimeDisplay,
-      'level': instance.level,
-      'distanceText': instance.distanceText,
-      'isLike': instance.isLike,
-      'imageCounts': instance.imageCounts,
-      'isCollected': instance.isCollected,
-      'collectCount': instance.collectCount,
-      'articleStatus': instance.articleStatus,
-      'poiName': instance.poiName,
+      'dynamicUrl': instance.dynamicUrl,
+      'originalUrl': instance.originalUrl,
     };
