dc8fb24707800377c04006d53b54441a280c4556
RockerHX <rockerhx@gmail.com>
2020-05-22 17:27:06 +0800

    Fix: 本地json文件异步读取的问题。
    读取文件最好用异步阻塞的方式，直接异步会导致先返回空。

--- lib/dao/travel_dao.dart
+++ lib/dao/travel_dao.dart
@@ -5,16 +5,8 @@ import 'package:flutter_trip/models/index.dart';
 
 class TravelDao {
   static Future<TravelModelResult> fetch() async {
-    try {
-      rootBundle.loadString('assets/data/travel.json').then(
-        (value) {
-          final result = json.decode(value);
-          return TravelModelResult.fromJson(result);
-        },
-      );
-    } catch (e) {
-      print(e);
-      return null;
-    }
+    final value = await rootBundle.loadString('assets/data/travel.json');
+    final result = json.decode(value);
+    return TravelModelResult.fromJson(result);
   }
 }
